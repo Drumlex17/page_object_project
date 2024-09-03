c932559ecab6139f7daac26805fa522d04988a20
Drumlex17 <sasha.levina.93@mail.ru>
2024-09-03 15:25:29 +0300

    test main page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
     def open(self):
         self.browser.get(self.url)
+    def is_element_present(self, how, what): # Создали новый метод, который будет перехватывать исключения. Передаём в него два аргумента (как искать - css, id, xpath и т.д.; и что искать - строку-селектор)
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException: # Тут указываем имя исключения
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,3 +4,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+    def should_be_login_link(self):
+        # self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid") # Специально здесь указали некорректный селектор
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented" # Добавили исключение и правильный селектор
